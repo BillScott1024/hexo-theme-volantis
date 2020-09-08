96b9211aed4ff738c2b2437094fd04c1fe612ded
Akimoto <jaxsonwang@foxmail.com>
2020-09-08 11:29:25 +0800

    [update]change message icon

--- source/js/aplayer.js
+++ source/js/aplayer.js
@@ -41,7 +41,7 @@ function setAPlayerObserver() {
 			var index = document.querySelector('meting-js').aplayer.list.index;
 			var title = document.querySelector('meting-js').aplayer.list.audios[index].title;
 			var artist = document.querySelector('meting-js').aplayer.list.audios[index].artist;
-			$.message({title: '音乐通知',message: '暂停播放：' + title + ' - ' + artist,type: 'success'});
+			$.message({title: '音乐通知',message: '暂停播放：' + title + ' - ' + artist,type: 'warning'});
 		});
         APlayerController.player.on('volumechange', function (e) {
 			onUpdateAPlayerVolume();
